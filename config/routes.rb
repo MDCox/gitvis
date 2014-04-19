a3170c59f6069580ef4032b4987592b6fe21516d
Matt Cox <mattcoxpdx@gmail.com>
2014-04-18 20:02:47 -0700

    added :people routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Gitvis::Application.routes.draw do
   root "main#index"
   resources :organizations, only: [:index, :create, :show]
   resources :main, only: :index
+  resources :people, only: [:index, :create, :show]
 end
