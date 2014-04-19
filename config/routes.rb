81405270bcb2ca012c7794ec46ebc82c33c0ea10
Matt Cox <mattcoxpdx@gmail.com>
2014-04-19 16:17:26 -0700

    removed redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Gitvis::Application.routes.draw do
   root "main#index"
-  resources :organizations, only: [:index, :create, :show]
-  resources :main, only: :index
+  resources :organizations, only: [:create, :show]
   resources :people, only: [:index, :create, :show]
 end
