6016159e22869a7088ccc20e60909071a83bff4a
Matt Cox <mattcoxpdx@gmail.com>
2014-04-18 19:05:26 -0700

    added alert on blank orginization :name

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,12 +3,23 @@ class OrganizationsController < ApplicationController
   end
 
   def new
-
   end
 
   def create
+    @org = Organization.new(organization_params)
+    if @org.save
+      redirect_to orginization_path(@org)
+    else
+      flash[:alert] = "Please enter an orginization name"
+      redirect_to root_path
+    end
   end
 
   def show
+  end
+
+private
+  def organization_params
+    params.require(:organization).permit(:name)
   end
 end
