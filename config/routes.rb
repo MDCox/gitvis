5bc2d1b62044ae885edc180ccc910d9d0f907bdc
Matt Cox <student@epicodus.com>
2014-04-18 17:24:19 -0700

    added controllers, views, and tests for organizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Gitvis::Application.routes.draw do
+  get "organizations/index"
+  get "organizations/new"
+  get "organizations/create"
+  get "organizations/show"
   resources :main, only: :index
 end
