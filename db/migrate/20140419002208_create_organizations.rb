a02c9f5464a725825126be99350ab71795111f3e
Matt Cox <student@epicodus.com>
2014-04-18 17:29:39 -0700

    added organization table

--- db/migrate/20140419002208_create_organizations.rb
+++ db/migrate/20140419002208_create_organizations.rb
@@ -1,6 +1,8 @@
 class CreateOrganizations < ActiveRecord::Migration
   def change
     create_table :organizations do |t|
+      t.string :name
+      t.string :description
 
       t.timestamps
     end
