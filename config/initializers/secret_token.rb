1739ba6512315f04e990d87b6dc10f9aaa7a50df
Matt Cox <student@epicodus.com>
2014-04-18 17:11:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitvis::Application.config.secret_key_base = '602f673ae2144186755f8b3d2f07958923210e6238a231fcb726a3a9886c5358a3d4b28030ebc24a1eacf4e7310901d5bccb5b08562a79c08094830268416f66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitvis::Application.config.session_store :cookie_store, key: '_gitvis_session'
