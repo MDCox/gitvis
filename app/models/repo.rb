64ff4f2536bf181b3e260fdea01d3917f5dac4f0
Matt Cox <mattcoxpdx@gmail.com>
2014-04-19 16:15:21 -0700

    removed terminal logging of empty repo error

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -5,10 +5,10 @@ class Repo < ActiveRecord::Base
   def build_commits
     begin
       commits = Octokit.commits(self.full_name)
-    rescue Octokit::Error => e
-      puts e.message
+    rescue Octokit::Error
       commits = []
     end
+    
     commits.each do |commit|
       Commit.create(repo_id: self.id,
                     author: commit[:commit][:author][:name],
