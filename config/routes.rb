caf2b87c9999b33c80fc0e30fe1d2df1e52fd2d9
Matt Cox <mattcoxpdx@gmail.com>
2014-04-21 06:12:45 -0700

    added show route for analysis

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Gitvis::Application.routes.draw do
   root "main#index"
   resources :organizations, only: [:create, :show]
   resources :people, only: [:index, :create, :show]
+  resources :analysis, only: [:show]
 end
