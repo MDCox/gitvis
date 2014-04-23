ef20e81e2196e7a64c15aaf74d01fbad12b07e9a
Matt Cox <mattcoxpdx@gmail.com>
2014-04-22 19:11:18 -0700

    organization data from db rendered to js in analysis view

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -1,5 +1,20 @@
 class AnalysisController < ApplicationController
   def show
     @organization = Organization.find(params[:id])
+    @people = @organization.people
+    @repos = []
+    @commits = []
+
+    @people.each do |person|
+      person.repos.each do |repo|
+        @repos << repo
+      end
+    end
+
+    @repos.each do |repo|
+      repo.commits.each do |commit|
+        @commits << commit
+      end
+    end
   end
 end
